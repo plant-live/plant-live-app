151f98bd3523cb7a3ed06326f80fc94bf99e1f15
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-22 17:32:49 -0300

    chore: config some examples

--- src/shared/components/Teste/index.tsx
+++ src/shared/components/Teste/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyledH1 } from './styles';
+import { ITeste } from './types';
 
-const Teste = () => {
+const Teste: ITeste = ({ titulo, texto }) => {
   return <StyledH1>Aqui é um H1 -0teste</StyledH1>;
 };
 
