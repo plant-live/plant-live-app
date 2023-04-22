633cf6018c0f792974c5669f3926a8f543fde83c
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-22 17:02:47 -0300

    feat: teste

--- src/shared/components/Teste/index.tsx
+++ src/shared/components/Teste/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledH1 } from './styles';
 
 const Teste = () => {
-  return <StyledH1>Aqui é um H1</StyledH1>;
+  return <StyledH1>Aqui é um H1 -0teste</StyledH1>;
 };
 
 export default Teste;
