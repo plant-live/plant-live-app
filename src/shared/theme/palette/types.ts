c900a0b8c62b4d443d991e1ab38e21582ab6949d
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-24 22:23:41 -0300

    config: make types optional for now, to later implementation]

--- src/shared/theme/palette/types.ts
+++ src/shared/theme/palette/types.ts
@@ -1,3 +1,39 @@
-export interface IPaletteOptions {
-  type: string;
+import {
+  PaletteOptions,
+  SimplePaletteColorOptions,
+  TypeBackground
+} from '@mui/material';
+
+export interface IPaletteOptions extends PaletteOptions {
+  type: 'light' | 'dark';
+  primary?: {
+    shade1: string;
+    shade2: string;
+    shade3: string;
+    shade4: string;
+  } & SimplePaletteColorOptions;
+  secondary?: SimplePaletteColorOptions;
+  text?: {
+    primary?: string;
+    secondary?: string;
+    disabled?: string;
+    light?: string;
+    dark?: string;
+    special?: string;
+  };
+  background?: {
+    secondary: string;
+  } & TypeBackground;
+  border?: string;
 }
+
+export interface IDefaultPalette extends PaletteOptions {
+  // Example of Populating IDefaultPalette
+  axes: {
+    x: string;
+    y: string;
+    z: string;
+  };
+}
+
+export type IDefaultPaleteOptions = PaletteOptions & IDefaultPalette;
