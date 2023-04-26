718f3bb79b22946e4360b1e1fa8e24591fea82ad
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-25 21:42:26 -0300

    feat: create some start theme files, but not implemented yet

---
+++ src/shared/theme/Dark.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material';
+import { yellow } from '@mui/material/colors';
+
+export const DarkTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#FBC02D',
+      contrastText: '#0A381E'
+    }
+  }
+});
---
+++ src/shared/theme/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1e88e5',
+      light: '#6ab7ff',
+      dark: '#005cb2'
+    },
+    secondary: {
+      main: '#e53935',
+      light: '#ff6f60',
+      dark: '#ab000d'
+    }
+
+    // outras cores personalizadas, se necessário
+  }
+});
