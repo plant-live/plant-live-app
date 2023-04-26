97768e6c249572f337fed27f97089026007897b2
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-25 21:45:26 -0300

    refactor: remove theme provider for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
 import Teste from '@/shared/components/Teste';
-import { ThemeSelectorV5 } from '@/shared/theme';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider } from '@mui/material/styles';
 
 export default function App() {
-  const themeProviderV5 = ThemeSelectorV5('dark');
-  return (
-    <ThemeProvider theme={themeProviderV5}>
-      <Teste titulo='OI' texto='Boa tarde' />
-    </ThemeProvider>
-  );
+  return <Teste titulo='OI' texto='Boa tarde' />;
 }
