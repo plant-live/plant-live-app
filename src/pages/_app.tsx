c9f53df37326120fb1c866ee56d47711f124fe07
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-29 20:33:36 -0300

    feat: create theme and update unit of 1rem of material ui 5

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import DashboardContainer from '@/shared/components/DashboardContainer';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { theme } from '@/shared/theme';
+import { ThemeProvider } from '@mui/material';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <DashboardContainer>
-      <Component {...pageProps} />
-    </DashboardContainer>
+    <ThemeProvider theme={theme}>
+      <DashboardContainer>
+        <Component {...pageProps} />
+      </DashboardContainer>
+    </ThemeProvider>
   );
 }
