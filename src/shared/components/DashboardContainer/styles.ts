e3e489ce6ff4016e158dd8cc31a0ec90f34e2b8c
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-29 16:32:43 -0300

    feat: update styles of dashboard container

--- src/shared/components/DashboardContainer/styles.ts
+++ src/shared/components/DashboardContainer/styles.ts
@@ -1,6 +1,9 @@
+import { darkColors } from '@/shared/theme/colors';
 import { styled, Container } from '@mui/material';
 
 export const StyledContainer = styled('div')(({ theme }) => ({
-  backgroundColor: '#182523',
+  backgroundColor: `${darkColors.background.primary}`,
+  display: 'flex',
+  flexDirection: 'column',
   height: '100vh'
 }));
