b338ba829cb987f6541eb0de2102b843277f5ff4
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-29 20:32:19 -0300

    chore: remove useless files

--- src/shared/components/Teste/styles.ts
+++
@@ -1,7 +0,0 @@
-import { styled, Grid } from '@mui/material';
-
-export const StyledH1 = styled('h1')(({ theme }) => ({
-  fontSize: '60px',
-  padding: theme.spacing(10),
-  color: theme.palette.primary.dark
-}));
