49085f7442a9be57b63d85741c3c989a7a8771b4
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-29 16:35:19 -0300

    feat: update colors of theme

--- src/shared/theme/colors/index.ts
+++ src/shared/theme/colors/index.ts
@@ -1,8 +1,18 @@
+import { IDarkColors } from './types';
+
 export const white = '#FFFFFF';
 export const black = '#000000';
 
-export const darkColors = {
+export const darkColors: IDarkColors = {
   text: {
-    primary: '#64A49B'
+    primary: '#64A49B',
+    white: '#fff',
+    button: {
+      secondary: '#64A49B'
+    }
+  },
+  background: {
+    primary: '#182523',
+    secondary: '#1E2D2B'
   }
 };
---
+++ src/shared/theme/colors/types.ts
@@ -0,0 +1,15 @@
+export interface IDarkColors {
+  text: {
+    primary: string;
+    white: string;
+    button: {
+      secondary: string;
+    };
+  };
+  background: {
+    primary: string;
+    secondary: string;
+  };
+}
+
+type SLA = () => number;
