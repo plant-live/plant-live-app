9cd5ffb648e42d6f2c0c16daafccf837c3ade848
Carlos Afonso <carlos.flach15@gmail.com>
2023-04-30 03:21:41 -0300

    feat: creating a custom login / sign up components

--- src/shared/components/CustomLoginSignUpComponents/index.tsx
+++ src/shared/components/CustomLoginSignUpComponents/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { StyledBodyWrapper } from './styles';
 import CustomTopBar from './components/CustomTopBar';
 import CustomBody from './components/CustomBody';
+import { ICustomLoginSingUpComponent } from './types';
 
-export const CustomLoginSingUpComponent = () => {
+export const CustomLoginSingUpComponent: ICustomLoginSingUpComponent = ({
+  checkboxText
+}) => {
   return (
     <StyledBodyWrapper>
       <CustomTopBar />
-      <CustomBody />
+      <CustomBody checkboxText={checkboxText} />
     </StyledBodyWrapper>
   );
 };
